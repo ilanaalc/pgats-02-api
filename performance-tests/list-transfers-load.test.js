466cb12e0d1f780d349dd95520bb32b3ff492953
Alsestrela <alsestrela@gmail.com>
2025-10-08 22:11:36 -0300

    incluindo o teste de performance GET/transfers

--- performance-tests/list-transfers-load.test.js
+++ performance-tests/list-transfers-load.test.js
@@ -9,12 +9,12 @@ export const options = {
     thresholds: {
         http_req_failed: ['rate<0.01'],
         http_req_duration: ['p(95)<200'],
-    }
-}
+    },
+};
 
 export default function () {
-  const resposta = http.get(pegarBaseURL() + '/transfers')
-  console.log(resposta)
+  const resposta = http.get(pegarBaseURL() + '/transfers');
+  //console.log(resposta)
   
   check (resposta, {
     'Validar que o status é 200': (r) => r.status ===200,
